a18120cd26dc920cac952b63c4c918486c1ee883
hiradsite <hiradgamer280@gmail.com>
2026-09-10 18:54:51 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-if [ ! -f paper.jar ]; then echo "[ERROR] paper.jar not found. Place Paper 1.16.5 as paper.jar here."; exit 1; fi
-java -Xms2G -Xmx6G -jar paper.jar nogui
+if [ ! -f paper.jar ]; then
+    echo "[ERROR] paper.jar not found. Place Paper 1.16.5 as paper.jar here."
+    exit 1
+fi
+
+java -Xms512M -Xmx3G -jar paper.jar nogui
